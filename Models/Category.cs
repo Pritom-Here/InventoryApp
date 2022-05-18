778ceddf1e94afe2d371b65a6f4c094aa8070c30
Pritom-Here <pritomchakraborty@outlook.com>
2022-05-18 09:03:57 +0600

    New Column Added to Category Entity. But Not Added to Migration Yet

--- Models/Category.cs
+++ Models/Category.cs
@@ -12,11 +12,19 @@ namespace InventoryApp.Models
         [MaxLength(50)]
         public string Name { get; set; }
 
+        [Required]
+        public byte CategoryType { get; set; }
+
         public string ParentId { get; set; }
 
         [ForeignKey("ParentId")]
         public Category Parent { get; set; }
 
         public List<Category> ChildCategories { get; set; }
+
+
+        public static readonly byte Primary = 1;
+        public static readonly byte Secondary = 2;
+        public static readonly byte Tertiary = 3;
     }
 }
